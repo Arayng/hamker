e0c4262261e45c1ab63d89477a8e137928f2e035
Arayng <akddnjf12@gmail.com>
2023-02-08 15:29:06 +0900

    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/calendar">
       <App />
     </BrowserRouter>
   </React.StrictMode>
